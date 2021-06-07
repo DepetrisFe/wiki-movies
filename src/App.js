06d7fc2bc015e5d2da2b1284ed2f07253cbae680
Federico Depetris <depetrisfe@gmail.com>
2021-06-07 16:11:07 -0300

    Actualizacion header Search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Portada from "./Components/Portada";
+import Search from "./Components/Search";
 
 function App() {
   return (
     <>
+      <Search />
       <Portada />
     </>
   );
---
+++ src/Components/Search.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <>
+      <header>
+        <h2>WIKI-MOVIES</h2>
+      </header>
+    </>
+  );
+};
+
+export default Search;
